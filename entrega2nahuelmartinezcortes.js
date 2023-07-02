76fe04aebe6bea3802f0fe97248bd493ebea6e0d
nahuel-martinez <65731857+nahuel-martinez@users.noreply.github.com>
2023-07-02 19:21:42 -0300

    Add files via upload

--- entrega2nahuelmartinezcortes.js
+++ entrega2nahuelmartinezcortes.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 class ProductManager {
     constructor(path) {
@@ -67,3 +68,68 @@ class ProductManager {
         return maxId + 1;
     }
 }
+
+const manager = new ProductManager('products.json');
+
+
+
+module.exports = {ProductManager}
+/*
+
+const ProductManager = require('./ProductManager');
+
+const productManager = new ProductManager('products.json');
+
+const product1 = {
+    title: 'Producto prueba 1',
+    description: 'Este es un producto prueba',
+    price: 200,
+    thumbnail: 'sin imagen',
+    code: 'ABC123',
+    stock: 25
+};
+
+const product2 = {
+    title: 'Producto prueba 2',
+    description: 'Descripción del producto 2',
+    price: 15,
+    thumbnail: 'ruta/imagen2.jpg',
+    code: 'XYZ789',
+    stock: 20
+};
+
+// Llamo a getProducts antes de cargar un producto y me da vacío
+const products = productManager.getProducts();
+console.log('Todos los productos:', products);
+
+// Agregar productos
+const productId1 = productManager.addProduct(product1);
+const productId2 = productManager.addProduct(product2);
+
+console.log('ID del producto 1:', productId1);
+console.log('ID del producto 2:', productId2);
+
+// Obtener todos los productos
+const allProducts = productManager.getProducts();
+console.log('Todos los productos:', allProducts);
+
+// Obtener un producto por ID
+const productById = productManager.getProductById(productId1);
+console.log('Producto por ID:', productById);
+
+// Actualizar un producto
+const updatedFields = { price: 12, stock: 60 };
+const isUpdated = productManager.updateProduct(productId1, updatedFields);
+console.log('Producto actualizado:', isUpdated);
+
+// Eliminar un producto
+const isDeleted = productManager.deleteProduct(productId2);
+console.log('Producto eliminado:', isDeleted);
+
+// Obtener todos los productos
+const allProducts1 = productManager.getProducts();
+console.log('Todos los productos:', allProducts1);
+
+*/
+
+//14601
---
+++ entreganueva.js
@@ -0,0 +1,134 @@
+import fs from 'fs';
+
+class ProductManager {
+  #products = [];
+  #lastId = 0;
+  #path = '';
+
+  constructor(path) {
+    this.#setPath(path);
+    console.info('ProductManager instance created');
+  }
+
+  #setPath(path) {
+    this.#path = path;
+    if ( fs.existsSync(this.#path) ) {
+      this.#loadProducts()
+    } else{
+      this.#saveFile();
+    }
+  }
+
+  #loadProducts() {
+    try {
+      const content = fs.readFileSync(this.path,'utf-8')
+      const { products, lastId } = JSON.parse(content);
+      this.#products = products;
+      this.#lastId = lastId;
+    } catch (error) {
+      console.error('Error loading file:', error);
+    }
+  }
+
+  #saveFile() {
+    const content = JSON.stringify({ products: this.#products, lastId: this.#lastId });
+    try {
+        fs.writeFileSync(this.#path, content)
+    } catch (error){
+        console.error('Error saving file:', error)
+    }
+  }
+
+  #isProductValid(product) {
+    return (
+      product &&
+      typeof product.id === 'undefined' &&
+      typeof product.title === 'string' &&
+      typeof product.description === 'string' &&
+      typeof product.price === 'number' &&
+      product.price >= 0 &&
+      Array.isArray(product.thumbnails) &&
+      typeof product.code === 'string' &&
+      typeof product.stock === 'number' &&
+      product.stock >= 0
+    );
+  }
+
+  #isProductCodeDuplicate(code) {
+    return this.#products.some((product) => product.code === code);
+  }
+
+  #generateProductId() {
+    return ++this.#lastId;
+  }
+
+  addProduct(product) {
+    if (!this.#isProductValid(product)) {
+      throw new Error('Invalid product');
+    }
+
+    this.#loadProducts();
+
+    if (this.#isProductCodeDuplicate(product.code)) {
+      throw new Error('Product with the same code already exists');
+    }
+
+    const id = this.#generateProductId();
+    const newProduct = { id, ...product };
+    this.#products.push(newProduct);
+
+    this.#saveFile();
+  }
+
+  getProducts() {
+    this.#loadProducts();
+    return this.#products;
+  }
+
+  getProductById(id) {
+    this.#loadProducts();
+
+    const product = this.#products.find((p) => p.id === id);
+
+    if (!product) {
+      throw new Error('Product not found');
+    }
+
+    return product;
+  }
+
+  deleteProduct(id) {
+    this.#loadProducts();
+
+    const productIndex = this.#products.findIndex((p) => p.id === id);
+
+    if (productIndex === -1) {
+      throw new Error('Product not found');
+    }
+
+    this.#products.splice(productIndex, 1);
+
+    this.#saveFile();
+  }
+
+  updateProduct(id, product) {
+    if (!this.#isProductValid(product)) {
+      throw new Error('Invalid product');
+    }
+
+    this.#loadProducts();
+
+    const productIndex = this.#products.findIndex((p) => p.id === id);
+
+    if (productIndex === -1) {
+      throw new Error('Product not found');
+    }
+
+    const updatedProduct = { id, ...product };
+    this.#products[productIndex] = updatedProduct;
+
+    this.#saveFile();
+  }
+}
+
+export default ProductManager;
